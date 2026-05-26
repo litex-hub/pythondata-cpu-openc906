c7c0472fd315875a16bab13d32e30d92a352efda
Florent Kermarrec <florent@enjoy-digital.fr>
2026-05-26 16:49:34 +0200

    packaging: fix release installs

--- setup.py
+++ setup.py
@@ -1,9 +1,15 @@
 import setuptools
+import re
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-from pythondata_cpu_openc906 import version_str
+with open("pythondata_cpu_openc906/__init__.py", "r") as fh:
+    version_str = re.search(
+        r'^version_str = "([^"]+)"',
+        fh.read(),
+        re.MULTILINE,
+    ).group(1)
 
 setuptools.setup(
     name="pythondata-cpu-openc906",
@@ -24,7 +30,7 @@ Python module containing verilog files for OpenC906 cpu.""",
     zip_safe=False,
     packages=setuptools.find_packages(),
     package_data={
-    	'cpu_openc906': ['cpu_openc906/verilog/**'],
+    	'pythondata_cpu_openc906': ['verilog/**'],
     },
     include_package_data=True,
     project_urls={
